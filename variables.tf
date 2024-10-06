5fef5b30579646cdc2b0b453c8bbdc274de7031d
cleophas <cleophasmashiri@gmail.com>
2024-10-06 13:27:25 +0200

    update

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  description = "The AWS region where resources will be created"
+  type        = string
+  default     = "eu-north-1"
+}
+
+variable "
+" {
+  description = "List of S3 bucket names to create"
+  type        = list(string)
+  default     = ["dashboard"]
+}
