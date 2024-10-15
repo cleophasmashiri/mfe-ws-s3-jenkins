de6a98c0a8636eda44bb6bbd6bc4c41c58c1f159
cleophas <cleophasmashiri@gmail.com>
2024-10-15 19:38:22 +0200

    update

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ resource "aws_s3_bucket" "multiple_buckets" {
 
   bucket = each.key
   force_destroy = true
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = true
+  restrict_public_buckets = false
 
   # Enable versioning (optional)
   versioning {
