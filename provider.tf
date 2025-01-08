3326838bb8500c5eb2610ea906dbf4aedf5e6f9e
cleophas <whaa9i3fc69e5tol6v63phn59cybdn@bots.bitbucket.org>
2025-01-08 16:16:10 +0200

    Update

--- provider.tf
+++ provider.tf
@@ -9,8 +9,8 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region                   = "eu-north-1"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "vscode"
+  # shared_credentials_files = ["~/.aws/credentials"]
+  # profile                  = "vscode"
 }
 
 # provider "aws" {
